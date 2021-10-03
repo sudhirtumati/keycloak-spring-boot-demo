3c4068a46b880fc2ba610365369f0323e62e6aff
Sudhir Tumati <mailofsudhir@gmail.com>
2021-10-03 13:08:50 +0800

    use SimpleAuthorityMapper

--- src/main/java/com/sample/security/webapp/config/SecurityConfig.java
+++ src/main/java/com/sample/security/webapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -27,7 +29,9 @@ public class SecurityConfig {
 
         @Autowired
         public void configureGlobal(AuthenticationManagerBuilder auth) {
-            auth.authenticationProvider(keycloakAuthenticationProvider());
+            KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+            keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+            auth.authenticationProvider(keycloakAuthenticationProvider);
         }
 
         @Override
@@ -64,7 +68,9 @@ public class SecurityConfig {
 
         @Autowired
         public void configureGlobal(AuthenticationManagerBuilder auth) {
-            auth.authenticationProvider(keycloakAuthenticationProvider());
+            KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+            keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+            auth.authenticationProvider(keycloakAuthenticationProvider);
         }
 
         @Override
